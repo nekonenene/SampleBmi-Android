4115fa6aa614463c3334ce94f071266020f72ec2
ハトネコエ hatonekoe@gmail.com <radio7954@gmail.com>
2015-11-03 07:22:32 +0900

    画面の骨組みは作った。

---
+++ app/src/test/java/hato_neko/x0/com/samplebmi/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package hato_neko.x0.com.samplebmi;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest
+{
+	@Test
+	public void addition_isCorrect() throws Exception
+	{
+		assertEquals( 4, 2 + 2 );
+	}
+}
