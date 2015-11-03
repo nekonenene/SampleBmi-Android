2fa59a9e7d715c9501e7134d24582346eb82cd3f
ハトネコエ hatonekoe@gmail.com <radio7954@gmail.com>
2015-11-04 08:10:18 +0900

    BMIを計算し、結果をテキストとして出力する機構を作た。

--- app/src/main/java/hato_neko/x0/com/samplebmi/MainActivity.java
+++ app/src/main/java/hato_neko/x0/com/samplebmi/MainActivity.java
@@ -1,13 +1,13 @@
 package hato_neko.x0.com.samplebmi;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,7 +19,12 @@ public class MainActivity extends AppCompatActivity
 		setContentView( R.layout.activity_main );
 		Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
 		setSupportActionBar( toolbar );
-		
+
+		TextView bmiLabel = (TextView)findViewById(R.id.bmiLabel) ;
+		System.out.println( bmiLabel.getText() ) ;
+		bmiLabel.setText( "" );
+		TextView commentLabel = (TextView)findViewById(R.id.commentLabel) ;
+		commentLabel.setText( "" );
 		/* メールボタンの処理
 		FloatingActionButton fab = (FloatingActionButton) findViewById( R.id.fab );
 		fab.setOnClickListener( new View.OnClickListener()
@@ -32,12 +37,60 @@ public class MainActivity extends AppCompatActivity
 		} );
 		*/
 	}
+
+	public int onCalcButton(View button)
+	{
+		Float height = 0f ;
+		Float weight = 0f ;
+		EditText heightField = (EditText)findViewById(R.id.heightField) ;
+		String heightValueString = heightField.getText().toString() ;
+		if(heightValueString.length() > 0)
+		{
+			Float heightCentiMeter = Float.parseFloat( heightValueString );
+			height = heightCentiMeter / 100;
+		}else{ return -1 ; }
+
+		EditText weightField = (EditText)findViewById(R.id.weightField) ;
+		String weightValueString = weightField.getText().toString() ;
+		if(weightValueString.length() > 0)
+		{
+			weight = Float.parseFloat( weightValueString );
+		}else{ return -1 ; }
+
+		if(height == 0)
+		{
+			return -1 ;
+		}
+		Float bmi = weight / (height * height) ;
+		System.out.println( "height = " + height + ", weight = " + weight + ", BMI = " + bmi ) ;
+
+		TextView bmiLabel = (TextView)findViewById(R.id.bmiLabel) ;
+		System.out.println( bmiLabel.getText() ) ;
+		bmiLabel.setText( "BMI : " + bmi ) ;
+
+		TextView commentLabel = (TextView)findViewById(R.id.commentLabel) ;
+		if(bmi < 17.6){
+			commentLabel.setText( "やせすぎです。\nもっと食べましょう。" ) ;
+		}else if(bmi < 19.8){
+			commentLabel.setText( "やせ気味ですが、まあ、いい感じですね。" ) ;
+		}else if(bmi < 24.2){
+			commentLabel.setText( "健康的なBMIです。\nいいっすね〜" ) ;
+		}else if(bmi < 26.4){
+			commentLabel.setText( "太り気味ですね。\nけっこうまずいので、体を動かしましょう。" ) ;
+		}else if(bmi >= 26.4){
+			commentLabel.setText( "やばいです。やばい。" ) ;
+		}else{
+			commentLabel.setText( "ERROR" ) ;
+		}
+
+		return 1 ;
+	}
+
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate( R.menu.menu_main, menu );
 		return true;
 	}
 	
--- app/src/main/java/hato_neko/x0/com/samplebmi/MainActivityFragment.java
+++ app/src/main/java/hato_neko/x0/com/samplebmi/MainActivityFragment.java
@@ -1,7 +1,7 @@
 package hato_neko.x0.com.samplebmi;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +14,14 @@ public class MainActivityFragment extends Fragment
 	
 	public MainActivityFragment()
 	{
+
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
+
 		return inflater.inflate( R.layout.fragment_main, container, false );
 	}
+
 }
